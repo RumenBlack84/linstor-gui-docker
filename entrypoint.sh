f316019da52b88420fcb71baac8ff2eecac6d081
RumenBlack84 <80719678+RumenBlack84@users.noreply.github.com>
2024-09-12 15:54:23 -0400

    Create entrypoint.sh
    clone, npm install, set .env variables

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Set the target directory for the clone
+TARGET_DIR="/config"
+
+# Check if the directory exists and is a git repository
+if [ ! -d "${TARGET_DIR}/.git" ]; then
+  echo "Repository not found in ${TARGET_DIR}. Cloning..."
+  git clone --branch ${VERSION} --single-branch https://github.com/username/repository.git "${TARGET_DIR}"
+else
+  echo "Repository already exists in ${TARGET_DIR}. Skipping clone."
+fi
+
+npm --prefix ${TARGET_DIR} install
+
+NODE_IP=$(hostname -I | awk '{print $1}')
+
+echo "**** Configuring /config/.env ****"
+cat <<EOF > /config/.env
+HOST=${NODE_IP}
+PORT=8080
+LINSTOR_API_HOST=${CONTROLLER_IP}:${CONTROLLER_PORT}
+GATEWAY_API_HOST=${GATEWAY_IP}:${GATEWAY_PORT}
+VSAN_API_HOST=${VSAN_IP}:${VSAN_PORT}
+
+EOF
